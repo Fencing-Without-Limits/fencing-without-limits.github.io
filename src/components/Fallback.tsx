a0c62ff80f96c1def31f2cdaa168bfcb2cb2ed8e
Alan Zheng <110870507+threeholestraw@users.noreply.github.com>
2025-10-23 02:19:46 -0400

    feat: vite config, header component, and various utilities

--- src/components/Fallback.tsx
+++ src/components/Fallback.tsx
@@ -1,7 +1,8 @@
 import fwlLogo from '/FWL Logo Black.png'
 
 export const LoadingFallback = () => (
-    <div className='w-full h-screen flex items-center justify-center'>
-        <img src={fwlLogo} className="w-3xs h-3xs" alt="FWL logo" />
+    <div className="w-full h-screen flex items-center justify-center flex-col text-center opacity-pulse">
+        <img src={fwlLogo} className="w-[200px] h-auto" alt="FWL logo" />
+        <span className='text-6xl text-black font-dm'>Loading...</span>
     </div>
 )
---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router'
+import { cn } from '../utils/cn'
+import fwlLogo from '/FWL Logo Black.png'
+
+export const Header = ({ className }: { className?: string }) => {
+    return (
+        <div className={cn('fixed z-[999] top-12 left-0 right-0 px-8 md:px-14 lg:px-20', className)}>
+            <div className="max-w-7xl mx-auto flex justify-between rounded-3xl backdrop-blur-md bg-[rgba(255,255,255,0.5)] pl-4 py-1">
+                <Link to="/" className='w-16 h-auto rounded-2xl'>
+                    <img src={fwlLogo} className="hover:drop-shadow-[0_0_6px_rgba(255,255,255,1)]" alt="FWL logo" />
+                </Link>
+                <nav className="w-full flex justify-end items-center text-black text-xl gap-3 rounded-2xl p-2 px-4 pointer-events-auto">
+                    <Link
+                        to="/about-us"
+                        className="max-md:hidden "
+                    >
+                        About Us
+                    </Link>
+                    <hr />
+                </nav>
+            </div>
+        </div>
+    )
+}
+
+export const HeaderSpace = () => {
+    return (
+        <div className="h-24" />
+    )
+}
