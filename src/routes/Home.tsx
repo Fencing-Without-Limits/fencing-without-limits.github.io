0ab63593ce140fde381ccd69b7e3dbf3ce41bdc8
Alan Zheng <110870507+threeholestraw@users.noreply.github.com>
2025-11-20 12:36:24 -0500

    feat: image carousel

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,14 +1,44 @@
-import type { FC } from 'react'
+import { useEffect, useState, type FC } from 'react'
 import { HashLink } from 'react-router-hash-link'
 import { HeaderSpace } from '../components/Header'
+import { cn } from '../utils/cn'
 
 export const Home: FC = () => {
+  const images = ['hero-western.jpeg', 'DSC07777.JPG', 'hero-london.jpeg']
+
+  const [curImageIdx, setCurImageIdx] = useState(0)
+  const [fade, setFade] = useState(false)
+
+  useEffect(() => {
+    console.log("effect triggered")
+    const interval = setInterval(() => {
+      setFade(true);
+      setTimeout(() => {
+        setFade(false);
+        setCurImageIdx((curImageIdx + 1) % images.length);
+      }, 1000); // Duration of fade transition
+    }, 7000); // Interval of image change cycle
+
+    return () => clearInterval(interval);
+  }, [images.length])
+
   return (
     <>
       <section id="hero" className="relative overflow-clip h-[95dvh] min-h-fit flex flex-col">
-        <div aria-label="Background Video" className="absolute top-0 z-0 min-w-full min-h-full h-[100%] w-auto">
-          <img src="/DSC07777.JPG" className="h-full w-full object-cover" alt="hero-photo" />
-          <div className="absolute inset-0 bg-gradient-to-b from-[rgba(0,0,0,0.3)] to-[rgba(0,0,0,0.7)]" />
+        <div aria-label="background" className="absolute top-0 z-0 min-w-full min-h-full h-[100%] w-auto">
+          <img
+            src={images[curImageIdx]}
+            className={cn("h-full w-full object-cover absolute", fade ? 'fade-out' : 'fade-in')}
+            alt="current-hero-photo"
+            style={{ transition: fade ? 'opacity 1s ease' : 'opacity 0s ease' }}
+          />
+          <img
+            src={images[(curImageIdx + 1) % images.length]}
+            className={cn("h-full w-full object-cover", fade ? 'fade-in' : 'fade-out')}
+            alt="next-hero-photo"
+            style={{ transition: fade ? 'opacity 0s ease' : 'opacity 1s ease' }}
+          />
+          <div className="absolute z-3 inset-0 bg-gradient-to-b from-[rgba(0,0,0,0.3)] to-[rgba(0,0,0,0.7)]" />
         </div>
         <div className="relative z-10 *:mb-6 px-8 md:px-14 lg:px-20 *:max-w-7xl *:mx-auto mb-8 mt-auto">
           <HeaderSpace />
@@ -21,7 +51,7 @@ export const Home: FC = () => {
             </h2>
           </div>
           <div>
-            <HashLink to="#apply" className="inline-block text-white text-xl bg-slate-700 rounded-md py-1.5 px-3">
+            <HashLink to="#apply" className="inline-block text-white text-xl bg-blue-600 rounded-md py-1.5 px-3">
               Host a Livestream
             </HashLink>
           </div>
