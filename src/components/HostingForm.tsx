af51f06acf153ea121f61d09997e1a1b65610d71
Alan Zheng <110870507+threeholestraw@users.noreply.github.com>
2026-01-03 00:01:45 -0500

    feat: finished homepage user flow

---
+++ src/components/HostingForm.tsx
@@ -0,0 +1,49 @@
+import type { FC } from "react"
+
+export const HostingForm: FC = () => {
+    const submitHandler: React.FormEventHandler<HTMLFormElement> = (event) => {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const clubName = formData.get('club-name')?.valueOf()
+        const location = formData.get('location')?.valueOf()
+        const dates = formData.get('dates')?.valueOf()
+        const context = formData.get('context')?.valueOf()
+        window.location.href = `mailto:fencingwithoutlimits@gmail.com
+        ?subject=FWL @ ${clubName}%0D%0A&body=Club / Organization Name: ${clubName}%0D%0A
+        Location: ${location}%0D%0A
+        Preferred Dates: ${dates}%0D%0A%0D%0A
+        ${context}`
+    }
+    return (
+        <form
+            onSubmit={submitHandler}
+            className='w-full p-8 bg-white rounded-md text-base
+            grid grid-cols-4 max-md:grid-cols-2 gap-4'
+        >
+            <div className="col-span-2 *:w-full">
+                <p className="text-lg pl-1">Club / Organization Name</p>
+                <input type="text" name="club-name" className="border-2 p-2 rounded-md">
+                </input>
+            </div>
+            <div className="col-span-2 *:w-full">
+                <p className="text-lg pl-1">Location</p>
+                <input type="text" name="location" className="border-2 p-2 rounded-md">
+                </input>
+            </div>
+            <div className="col-span-4 *:w-full">
+                <p className="text-lg pl-1">Preferred Dates</p>
+                <input type="text" name="dates" className="border-2 p-2 rounded-md">
+                </input>
+            </div>
+            <div className="col-span-4 *:w-full">
+                <p className="text-lg pl-1">Additional Context</p>
+                <textarea name="context" rows={4} className="border-2 p-2 rounded-md">
+                Hello! My club is interested in hosting a FWL charity livestream.
+                </textarea>
+            </div>
+            <button type="submit" className="text-white text-xl bg-blue-600 rounded-md py-1.5 px-3" >
+                Send
+            </button>
+        </form>
+    )
+}
--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,11 +1,17 @@
+import { useScroll } from "motion/react"
 import { Outlet } from "react-router"
 import { Header } from "../components/Header"
+import { Footer } from "../components/Footer"
+import { useRef } from "react"
 
 export const Layout = () => {
+    const ref = useRef<HTMLDivElement>(null)
+    const { scrollY } = useScroll({ container: ref })
     return (
-        <div className="font-dm min-w-lg">
-            <Header />
+        <div ref={ref} className="font-dm min-w-lg h-dvh overflow-auto">
+            <Header scrollY={scrollY}/>
             <Outlet />
+            <Footer />
         </div>
     )
 }
